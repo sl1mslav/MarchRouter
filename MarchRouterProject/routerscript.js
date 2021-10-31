cbb0cc720cb15d6cb920bebca17126d8dbba001f
sl1mslav <dm.partilov@gmail.com>
2021-11-01 00:01:13 +0500

    added dedicated js, images folders; changes to webmap1_1.html

--- MarchRouterProject/routerscript.js
+++
@@ -1,9 +0,0 @@
-function hideAndShow(id) {
-    var lines = document.getElementsByClassName("line")
-    for (i = 0; i < lines.length; i++) {
-        lines[i].style.opacity = "0"    
-    }
-    document.getElementsByClassName("svgContainer")[0].style.opacity="1";
-    document.getElementById(id).classList.add("line");   
-}
-
